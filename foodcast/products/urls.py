0b117337477452ba1635b2d2cce9304082f5db8c
timxt23 <112951950+timxt23@users.noreply.github.com>
2023-09-27 18:22:30 +0300

    Добавлены все ручки по базовым моделям для CRUD

--- foodcast/products/urls.py
+++ foodcast/products/urls.py
@@ -1,12 +1,16 @@
 from django.urls import include, re_path
 from rest_framework.routers import DefaultRouter
 
-from .views import ShopsViewSet
+from .views import ShopsViewSet, GroupViewSet, CategoryViewSet, SubCategoryViewSet, ProductViewSet
 
 app_name = 'products'
 
 router = DefaultRouter()
 router.register(r'shops', ShopsViewSet, basename='shops')
+router.register(r'groups', GroupViewSet, basename='groups')
+router.register(r'category', CategoryViewSet, basename='category')
+router.register(r'subcategory', SubCategoryViewSet, basename='category')
+router.register(r'product', ProductViewSet, basename='product')
 
 urlpatterns = [
     re_path(
--- foodcast/products/views.py
+++ foodcast/products/views.py
@@ -1,11 +1,12 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from .filters import ShopFilter
-from .models import Shops
-from .serializers import ShopsSerializer
+from .models import Shops, ProductGroup, ProductCategory, ProductSubCategory, Product
+from .serializers import ShopsSerializer, GroupSerializer, CategorySerializer, SubCategorySerializer, ProductSerializer
 
 
 @action(detail=True, methods=['get'])
@@ -14,6 +15,37 @@ class ShopsViewSet(viewsets.ModelViewSet):
     queryset = Shops.objects.all()
     filter_backends = [DjangoFilterBackend,]
     filterset_class = ShopFilter
+    permission_classes = (IsAuthenticated,)
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        response_data = {'data': serializer.data}
+        return Response(response_data)
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    serializer_class = GroupSerializer
+    queryset = ProductGroup.objects.all()
+    permission_classes = (IsAuthenticated,)
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    serializer_class = CategorySerializer
+    queryset = ProductCategory.objects.all()
+    permission_classes = (IsAuthenticated,)
+
+
+class SubCategoryViewSet(viewsets.ModelViewSet):
+    serializer_class = SubCategorySerializer
+    queryset = ProductSubCategory.objects.all()
+    permission_classes = (IsAuthenticated,)
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+    permission_classes = (IsAuthenticated,)
 
     def list(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
