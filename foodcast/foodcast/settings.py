07522b24c3efe757ecfe5dc145f38e5da027df0d
kubanez-create <zo22@mail.ru>
2023-09-28 12:08:02 +0400

    redid migrations and added dinamic documentation

--- foodcast/foodcast/settings.py
+++ foodcast/foodcast/settings.py
@@ -17,6 +17,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_yasg',
     'rest_framework.authtoken',
     'rest_framework',
     'djoser',
