4d3a7ad5248b6351c04ec7f0599c21471738b125
kubanez-create <zo22@mail.ru>
2023-10-06 18:21:08 +0400

    added dates filter test, currently fails

--- foodcast/tests/test_filters.py
+++ foodcast/tests/test_filters.py
@@ -254,3 +254,22 @@ class ForecastFilterTests(TestCase):
         )
         response_json = json.loads(response.content).get("data")[0]
         self.assertEqual(response_json["sku"], ForecastFilterTests.product.sku)
+
+    def test_forecast_view_returns_correct_datapoints_by_date(self):
+        """Запрос на /forecast с фильтром по дате выдает верные данные.
+
+        Когда мы фильтруем по дате начала и окончания периода
+        date_after and date_before мы должны получить только
+        данные, которые находятся внутри временного диапазона
+        включая даты начала и окончания."""
+        response = self.authorized_client.get(
+            (
+                reverse("products:forecast-list", kwargs={"version": "v1"})
+                + f"?date_before={date(2023, 9, 1)}"
+                + f"&date_after={date(2023, 9, 3)}"
+            )
+        )
+        response_json = json.loads(response.content).get("data")[0]
+        print(response_json)
+        print(type(json.loads(response_json["sales_units"])))
+        self.assertEqual(len(response_json["sales_units"]), 3)
